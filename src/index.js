8a4a0bf7d02b8e1354fee925a27c425a6100ac2c
Natalya <natalya.gulak.apasova@yandex.by>
2019-03-14 22:57:24 +0300

    feat: create a method makeExchange that determines the minimum number of coins needed to make change for a given amount in American currency

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency <= 0 ) {
+    	return {};
+    }
+    if (currency >= 10000) {
+    	return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+    let value;
+    let coins = {
+		'H':50,
+		'Q':25,
+		'D':10,
+		'N':5,
+		'P':1
+    }
+    let result = {};
+    for (let key in coins) {
+    	if (currency >= coins[key]) {
+    		result[key] = Math.floor(currency / coins[key]);
+			currency = currency - result[key]*coins[key];
+    	} else {
+    		continue
+    	}
+	}
+	return result;
 }
